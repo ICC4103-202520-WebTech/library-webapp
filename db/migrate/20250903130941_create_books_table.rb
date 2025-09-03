819c6606805d05572e14b778511aabfff582b76c
Andrés Howard <ahowardm@gmail.com>
2025-09-03 09:22:28 -0400

    migration to add books table

---
+++ db/migrate/20250903130941_create_books_table.rb
@@ -0,0 +1,10 @@
+class CreateBooksTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.date :publication_date, null: false
+
+      t.timestamps
+    end
+  end
+end
