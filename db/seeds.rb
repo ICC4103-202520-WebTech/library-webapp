93a327b3272813f4a1ba6fadf9ccfbc51c49b278
Andrés Howard <ahowardm@gmail.com>
2025-09-17 09:33:15 -0300

    create association between books and authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Book.create(title: "Refactoring", publication_date: Date.new(2000, 5, 5))
+
+jordan_peterson = Author.create! first_name: "Jordan", last_name: "Peterson" 
+juan_perez = Author.create! first_name: "Juan", last_name: "Pérez"
+martin_fowler = Author.create! first_name: "Martin", last_name: "Fowler"
+
+refactoring = Book.create! title: "Refactoring", publication_date: Date.new(2000, 5, 5), author_id: martin_fowler.id
+papelucho = Book.create! title: "Papelucho", publication_date: Date.new(1960, 5, 5), author_id: juan_perez.id
+
