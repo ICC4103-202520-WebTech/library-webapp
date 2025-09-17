ac7d5da21b00362d7cd9c8f698cf1a1024023447
Andrés Howard <ahowardm@gmail.com>
2025-09-17 09:45:25 -0300

    add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,9 @@ class Author < ApplicationRecord
 
   has_many :books
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def full_name
     "#{first_name} #{last_name}"
   end
