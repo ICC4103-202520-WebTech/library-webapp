0d2ea6d982f64de9819cdeda96960407d342e236
Andrés Howard <ahowardm@gmail.com>
2025-09-17 10:18:41 -0300

    add callbacks

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,17 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show]
+
+  # /authors
   def index
     @authors = Author.all
   end
 
+  # authors/:id
   def show
+  end
+
+
+  def set_author
     @author = Author.find(params[:id])
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -5,7 +5,20 @@ class Author < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  before_validation :strip_name
+  before_validation :titleize_name
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def strip_name
+    self.first_name = first_name.strip
+    self.last_name = last_name.strip
+  end
+
+  def titleize_name
+    self.first_name = first_name.titleize
+    self.last_name = last_name.titleize
   end
 end
